f4547ae1a539e916570c081ec964b4652e374841
Marvellous1 <aigbemarvellous01@gmail.com>
2020-12-17 23:52:32 +0100

    Done finally

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_133331) do
+ActiveRecord::Schema.define(version: 2020_12_17_153652) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "name"
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_133331) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
